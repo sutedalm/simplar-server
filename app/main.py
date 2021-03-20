911a5acaa3b05b458d25aafebfcf4ecf48e97e7e
Masha <masha@mashahost.localdomain>
2021-03-20 21:49:28 +0100

    with gpt3 and access

--- app/main.py
+++ app/main.py
@@ -1,41 +1,76 @@
 import flask
+
 from flask import request, jsonify
 
+from access.preprocessors import get_preprocessors
+from access.resources.prepare import prepare_models
+from access.simplifiers import get_fairseq_simplifier, get_preprocessed_simplifier
+from access.text import word_tokenize
+from access.utils.helpers import yield_lines, write_lines, get_temp_filepath, mute
+import os
+import openai
+        
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-
-MOCK_DATA = {
-    'data': ["How to avoid the",
-             "dreaded public relations crisis",
-             "Bob Pickard",
-             "Opinion",
-             "Most important, show communication of the leader",
-             "F",
-             "acebook is an example of a new public relations reality.",
-             "Public relations means to manage communication:",
-             "The communication between an organization and others.",
-             "When something goes wrong for a company, everyone looks to the leader.",
-             "When the leader is gone, or the leader has nothing to say,",
-             "no information is there. This is called an information vacuum.",
-             "When an information vacuum happens, people on social media say,",
-             "the company is wrong. If this happens, these people are in the centre",
-             "of public relations. The opinion of these people suddenly",
-             "becomes a general wisdom."]
-}
+# A route to return all of the available entries in our catalog.
 
 
-@ app.route('/', methods=['GET'])
-def index():
-    return "<h1>Hello World</h1>"
-
-
-@ app.route('/api', methods=['POST'])
+@app.route('/', methods=['POST'])
 def api_all():
-    # body = request.json
-    # print(body)
-    # return jsonify(body['data'])
-    return jsonify(MOCK_DATA)
+    body = request.json
+    print(body['data'])
+    input = body['data']
+    ai = body["ai"]
+    if ai == "gpt3":
+        result = gpt3(result)
+    else:
+        result = evaluate(input)
+    
+    return jsonify(result)
+
+
+def evaluate(input):
+    # Usage: python generate.py < my_file.complex
+    # Read from stdin
+    source_filepath = get_temp_filepath()
+    write_lines([word_tokenize(line) for line in input], source_filepath)
+
+    # Load best model
+    best_model_dir = prepare_models()
+    recommended_preprocessors_kwargs = {
+        'LengthRatioPreprocessor': {'target_ratio': 0.95},
+        'LevenshteinPreprocessor': {'target_ratio': 0.75},
+        'WordRankRatioPreprocessor': {'target_ratio': 0.75},
+        'SentencePiecePreprocessor': {'vocab_size': 10000},
+    }
+
+    preprocessors = get_preprocessors(recommended_preprocessors_kwargs)
+    simplifier = get_fairseq_simplifier(best_model_dir, beam=8)
+    simplifier = get_preprocessed_simplifier(simplifier, preprocessors=preprocessors)
+    # Simplify
+    pred_filepath = get_temp_filepath()
+
+    with mute():
+        simplifier(source_filepath, pred_filepath)
+
+    return [line for line in yield_lines(pred_filepath)]
+
+
+def gpt3(input):
+    openai.api_key = os.environ["GPT3_KEY"]
+    response = openai.Completion.create(
+    engine="davinci",
+    prompt="My second grader asked me what this passage means:\n\"\"\"\n" + input + "\n\"\"\"\nI rephrased it for him, in plain language a second grader can understand:\n\"\"\"\n",
+    temperature=0,
+    max_tokens=60,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.0,
+    stop=["\"\"\""]
+    )
+
+    return response
 
 
 if __name__ == "__main__":
